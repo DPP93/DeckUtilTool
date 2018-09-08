d31e1cc7be3ae1f6cc2dc8877f517b66f6d845ae
dpp <dpeczek93@gmail.com>
2018-09-08 12:31:03 +0200

    Refined knowledge about databases

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var multer = require('multer');
 var upload = multer();
 var app = express();
 
-var cardRouter = require("./routers/cardRouter.js");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mtgDB', { useNewUrlParser: true });
+
+// var cardRouter = require("./routers/cardRouter.js");
+
+var cardSchema = mongoose.Schema({
+  title: String
+});
+
+var Card = mongoose.model("Card", cardSchema);
 
 app.use(express.static('public'));
 app.use("/static", express.static('public2'));
@@ -27,14 +36,59 @@ app.get('/form', function(req, res) {
 
 //Parsing application/json
 app.use(bodyParser.json());
+//Allowing to use a qs library
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(upload.array());
 
 app.post('/form', function(req, res) {
-  console.log(req);
+  console.log(req.body);
   res.send("Received your request");
 });
 
-app.use('/cards', cardRouter)
+app.get("/card", function(req, res) {
+  res.render("card");
+});
+
+app.post("/card", function(req, res) {
+  //Retrieve the information from form
+  console.log(req.body);
+  var cardInfo = req.body;
+  console.log(cardInfo.title);
+  if (!cardInfo.title) {
+    res.render('show_message',
+    {
+      message: "You didn't provide a name of the card",
+      type: "error"
+    });
+  }
+  else {
+    var newCard = new Card({
+      title: cardInfo.title
+    });
+    newCard.save(function(err, Card) {
+      if (err) {
+        res.render('show_message',
+        {
+          message: "Database error",
+          type: "error"
+        });
+      }
+      else {
+        res.render('show_message',
+        {
+          message: "New card added",
+          type: "error"
+        });
+      }
+    });
+  }
+});
+
+app.get("/getCard", function(req, res) {
+  Card.find(function(err, response){
+   console.log(response);
+});
+})
+// app.use('/cards', cardRouter)
 
 app.listen(3000);
